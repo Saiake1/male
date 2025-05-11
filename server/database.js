fd10b63405af205fe5331433e791a3251592af57
rahnumat <rahnumat@ut.ee>
2025-05-11 19:30:58 +0300

    database.js test

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'parool',
-  database: process.env.DATABASE || 'male',
+  database: process.env.DATABASE || 'male23',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || '5432',
 });
